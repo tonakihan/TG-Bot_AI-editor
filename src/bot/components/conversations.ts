bdadc9b82780c38711bae010e1047e233992afb3
tonakihan <tonakihan@gmail.com>
2026-03-29 12:55:35 +0300

    Fix conversation - cancel button

--- src/bot/components/conversations.ts
+++ src/bot/components/conversations.ts
@@ -6,9 +6,15 @@ import type { MyContext } from "../types/MyContext.d.ts";
 
 //TODO: Make possible use username in caption
 export async function captionSet(cvst: Conversation, ctx: Context) {
-  const cloneReturnMenu = cvst.menu("menu-return").text("<= Back to settings");
+  const cloneOrigMenu = cvst.menu("menu-settings-caption")
+    .text("Set text").row()
+    .text("Disable caption").row()
+    .text("<= Back");
+  const cloneReturnMenu = cvst.menu("menu-return")
+    .text("<= Back to settings");
 
   const menu = cvst.menu().text("Cancel", async (ctx) => {
+    await ctx.editMessageText("Caption:");
     await ctx.menu.nav("menu-settings-caption", { immediate: true });
     await cvst.halt();
   });
@@ -18,7 +24,7 @@ export async function captionSet(cvst: Conversation, ctx: Context) {
     reply_markup: menu,
   });
 
-  await ctx.reply("What is your text?");
+  await ctx.reply("Write your caption to me");
   const input = await cvst.form.text();
   await cvst.external(
     (ctx: MyContext) => (ctx.session.config.caption = input)
