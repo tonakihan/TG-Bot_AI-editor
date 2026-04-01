a8536bb179548df823500b82d69bb076f594f2d6
tonakihan <tonakihan@gmail.com>
2026-04-01 21:15:04 +0300

    Fix enter in middlewares/parserCaption.ts
    Sometimes happend the calls without ctx.msg.chat - I didn't uderstand
    why it is going. The logs printing that ctx.msg.chat is not exist because
    the settion key is undefined.

--- src/bot/middlewares/parserCaption.ts
+++ src/bot/middlewares/parserCaption.ts
@@ -3,21 +3,22 @@ import type { MiddlewareFn } from "grammy";
 import type { MyContext } from "../types/MyContext.d.ts";
 
 const parserCaption: MiddlewareFn<MyContext> = async (ctx, next) => {
-  const config = ctx.session.config;
-  if (
-    !config.caption ||
-    ctx.msg.chat.type === "private"
-  ) {
-    next();
-    return;
+  try {
+    const config = ctx.session.config;
+    if (!config.caption || ctx.msg.chat.type === "private") {
+      next();
+      return;
+    }
+
+    const username = ctx.from.username;
+    let caption = config.caption;
+    caption = caption.replaceAll(/\$username/g, "@" + username);
+
+    ctx.session.parsedCaption = caption;
+  } catch (e) {
+    console.error("Err: " + e);
   }
-
-  const username = ctx.from.username;
-  let caption = config.caption;
-  caption = caption.replaceAll(/\$username/g, "@" + username);
-
-  ctx.session.parsedCaption = caption;
   next();
-}
+};
 
 export default parserCaption;
